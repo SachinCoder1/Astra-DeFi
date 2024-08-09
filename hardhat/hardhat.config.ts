04ac685050deee933b56db067f5a208da0eeea3d
Sachin Gurjar <sachinhlo232@gmail.com>
2024-08-09 17:13:33 +0530

    contract deployed using mode testnet and verified

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,26 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
+import dotenv from "dotenv";
+dotenv.config();
+
+const MODE_SEPOLIA_PRIVATE_KEY = process.env.MODE_SEPOLIA_PRIVATE_KEY as string;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    modeTestnet: {
+      url: "https://sepolia.mode.network",
+      chainId: 919,
+      accounts: [MODE_SEPOLIA_PRIVATE_KEY],
+    },
+    modeMainnet: {
+      url: "https://mainnet.mode.network/",
+      chainId: 34443,
+      accounts: [MODE_SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  sourcify: { enabled: true },
 };
 
 export default config;
