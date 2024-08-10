198a73beaedf61cb0d2a0ce713693759235eaa1c
Sachin Gurjar <sachinhlo232@gmail.com>
2024-08-10 16:47:32 +0530

    faucet page (for getting ATX tokens initially... ideally the ATX will only be available to some dex decentralized exchanges..  but for now this is fine )

---
+++ client/src/app/(main)/faucet/page.tsx
@@ -0,0 +1,30 @@
+import Faucet from "@/components/faucet";
+import { Heading } from "@/components/ui/Typography";
+import React from "react";
+
+type Props = {};
+
+const Page = (props: Props) => {
+  return (
+    <div>
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-background px-10 pt-6 pb-4 rounded-xl shadow-xl max-w-lg w-full border">
+          <Heading variant="h2" className="text-center mb-4 mx-auto">
+            STAKING DTX FAUCET
+          </Heading>
+          <div className="text-center mb-6">
+            <p className="text-muted-foreground">
+              Staking tokens (DTX):{" "}
+              <span className="font-semibold">10 Staking DTX/day.</span>
+            </p>
+            <div className="mt-8">
+              <Faucet />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
